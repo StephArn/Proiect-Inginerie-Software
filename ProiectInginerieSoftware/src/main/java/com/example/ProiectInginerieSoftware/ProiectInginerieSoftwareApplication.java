6db101873826e5adc30d2a95220473fd6679baac
ablocku <45084824+ablocku@users.noreply.github.com>
2022-01-24 19:45:22 +0200

    setup + mqtt (not done)

---
+++ ProiectInginerieSoftware/src/main/java/com/example/ProiectInginerieSoftware/ProiectInginerieSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProiectInginerieSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectInginerieSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectInginerieSoftwareApplication.class, args);
+	}
+
+}
---
+++ ProiectInginerieSoftware/src/test/java/com/example/ProiectInginerieSoftware/ProiectInginerieSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProiectInginerieSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectInginerieSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
